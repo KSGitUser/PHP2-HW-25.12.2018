1076c6dd424cced9470f841233f5b11a6342937a
KSFirstUser <rusakk@mail.ru>
2019-01-08 16:31:27 +0300

    last edition HW2

--- HW2/models/DigitalProduct.php
+++ HW2/models/DigitalProduct.php
@@ -4,12 +4,19 @@ namespace app\models;
 
 class DigitalProduct extends Product
 {
-    public function __construct($id = null, $name = null, $description = null, $price = null, $vendor_id = null) {
+    public function __construct(
+      $id = null, 
+      $name = null, 
+      $description = null, 
+      $price = null, 
+      $vendor_id = null) 
+    {
       parent::__construct($id, $name, $description, $price, $vendor_id);
       $this->price = $this->getPrice();
     }
 
-    public function getPrice() {
+    public function getPrice() 
+    {
       return $this->price/2;
     }
 
